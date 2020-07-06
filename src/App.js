2c6ed44c77558377455d1f07c6b8126b64b5b247
aeonincode <khashayar.sasannia@hotmail.com>
2020-07-06 14:26:31 +0200

    Context in Class Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-// import Movie from './hoc/Movie';
-import Counter from './hooks/Counter';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// // import Movie from './hoc/Movie';
+// //import Counter from './hooks/Counter';
+// import Users from './hooks/Users';
 
-function App() {
-  //return <Movie id={1} />;
-  return <Counter />;
+// function App() {
+//   //return <Movie id={1} />;
+//   //return <Counter />;
+//   return <Users />;
+// }
+
+// export default App;
+
+import React, { Component } from 'react';
+import MoviePage from './context/MoviePage';
+import UserContext from './context/userContext';
+
+class App extends Component {
+  state = { currentUser: { name: 'Xerx' } };
+
+  render() {
+    return (
+      <UserContext.Provider value={this.state.currentUser}>
+        <div>
+          <MoviePage />
+        </div>
+      </UserContext.Provider>
+    );
+  }
 }
 
 export default App;
---
+++ src/context/MovieList.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import UserContext from './userContext';
+
+class MovieList extends Component {
+  static contextType = UserContext;
+
+  componentDidMount() {
+    console.log('context', this.context);
+  }
+
+  render() {
+    return (
+      <UserContext.Consumer>
+        {(UserContext) => <div>Movie List {UserContext.name}</div>}
+      </UserContext.Consumer>
+    );
+  }
+}
+
+//MovieList.contextType = UserContext;
+
+export default MovieList;
